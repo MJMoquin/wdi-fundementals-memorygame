cc6062de0032bac502c6d2f316af59882c63c895
MJ Moquin <mjmoquin@gmail.com>
2020-02-20 20:27:42 -0600

    created game logic functions

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,19 +1,22 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
 
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("Winner, winner, chicken dinner!")
-  }
-  else {
-    alert("Not a match. Try again.")
+function checkForMatch () {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("Winner, winner, chicken dinner!")
+    }
+    else {
+      console.log("Not a match. Try again.")
+    }
   }
 }
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
+function flipCard (cardID) {
+  cardsInPlay.push(cards[cardID]);
+  console.log("User flipped " + cards[cardID]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
