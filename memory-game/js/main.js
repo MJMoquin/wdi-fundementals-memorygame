622fb79e1cc0ffe56c5d492dae356eefd46ee250
MJ Moquin <mjmoquin@gmail.com>
2020-02-22 19:20:49 -0600

    ceated game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,22 +1,46 @@
-let cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
 let cardsInPlay = [];
 
 function checkForMatch () {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("Winner, winner, chicken dinner!")
+      alert("Winner, winner, chicken dinner!")
     }
     else {
-      console.log("Not a match. Try again.")
+      alert("Not a match. Try again.")
     }
   }
 }
 
 function flipCard (cardID) {
-  cardsInPlay.push(cards[cardID]);
-  console.log("User flipped " + cards[cardID]);
+  cardsInPlay.push(cards[cardID].rank);
+  console.log("User flipped " + cards[cardID].rank);
+  console.log(cards[cardID].cardImage);
+  console.log(cards[cardID].suit);
   checkForMatch();
 }
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
