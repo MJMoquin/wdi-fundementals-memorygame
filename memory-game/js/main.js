a0f0759bc2e53853497865a0dbba5c9bf1beedf5
MJ Moquin <mjmoquin@gmail.com>
2020-02-24 08:05:49 -0600

    ceated game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -22,11 +22,33 @@ const cards = [
 ];
 
 let cardsInPlay = [];
+let wins = 0;
+
+function resetBoard() {
+  let cardBoard = document.getElementById('game-board');
+  while (cardBoard.firstChild) {
+    cardBoard.removeChild(cardBoard.lastChild);
+  }
+  cardsInPlay = [];
+}
+
+function createBoard () {
+  resetBoard();
+  for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
 
 function checkForMatch () {
+  document.getElementsByTagName('img')[cardID].setAttribute('src', cards[cardID].cardImage)
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("Winner, winner, chicken dinner!")
+      wins += 1;
+      alert("Winner, winner, chicken dinner! Total wins: " + wins);
     }
     else {
       alert("Not a match. Try again.")
@@ -34,7 +56,9 @@ function checkForMatch () {
   }
 }
 
-function flipCard (cardID) {
+function flipCard () {
+  cardID = this.getAttribute('data-id');
+  console.log(cardID);
   cardsInPlay.push(cards[cardID].rank);
   console.log("User flipped " + cards[cardID].rank);
   console.log(cards[cardID].cardImage);
@@ -42,5 +66,4 @@ function flipCard (cardID) {
   checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
